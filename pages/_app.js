f582774bbac62a360b110dfa20f6b405ce236aeb
Jason Gong <jasongong83@gmail.com>
2021-07-24 17:11:54 -0400

    add home page with nav bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <p className="text-4xl font-bold">Metaroad Marketplace</p>
+        <div className="flex mt-8">
+          <Link href="/">
+            <a className="mr-6 text-purple-500">Home</a>
+          </Link>
+          <Link href="/create-item">
+            <a className="mr-6 text-purple-500">Sell NFT</a>
+          </Link>
+          <Link href="/my-assets">
+            <a className="mr-6 text-purple-500">My NFTs</a>
+          </Link>
+          <Link href="/creator-dashboard">
+            <a className="mr-6 text-purple-500">Creator Dashboard</a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;

43f75b2f56234436dbf17e4718e7cbe5a195fb6d
Jason Gong <jasongong83@gmail.com>
2021-07-26 12:08:51 -0400

    add create-item page. add imports and set initial state

---
+++ pages/create-item.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { ethers } from "ethers";
+import { create as ipfsHttpClient } from "ipfs-http-client";
+import { useRouter } from "next/router";
+import { Web3Modal } from "web3modal";
+
+const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
+
+import { nftaddress, nftmarketaddress } from "../config.js";
+
+import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
+import Market from "../artifacts/contracts/NFTMarket.sol/NFTMarket.json";
+
+export default function CreateItem() {
+  const [fileUrl, setFileUrl] = useState(null);
+  const [formInput, updateFormInput] = useState({
+    price: "",
+    name: "",
+    description: "",
+  });
+  const router = useRouter();
+}

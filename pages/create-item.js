57cef1dd1a860fe7fef50832c35ff5041788f08d
Jason Gong <jasongong83@gmail.com>
2021-07-27 19:40:48 -0400

    add createSale function that interacts with contracts to create token and add token to marketplace

--- pages/create-item.js
+++ pages/create-item.js
@@ -50,4 +50,29 @@ export default function CreateItem() {
       console.log("Error uploading file: ", error);
     }
   }
+
+  async function createSale(url) {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+
+    let contract = new ethers.Contract(nftaddress, NFT.abi, signer);
+    let transaction = await contract.createToken(url);
+    let tx = await transaction.wait();
+    let event = tx.events[0];
+    let value = event.args[2];
+    let tokenId = value.toNumber();
+    const price = ethers.utils.parseUnits(formInput.price, "ether");
+
+    contract = new ethers.Contract(nftmarketaddress, Market.abi, signer);
+    let listingPrice = await contract.getListingPrice();
+    listingPrice = listingPrice.toString();
+
+    transaction = await contract.createMarketItem(nftaddress, tokenId, price, {
+      value: listingPrice,
+    });
+    await transaction.wait();
+    router.push("/");
+  }
 }

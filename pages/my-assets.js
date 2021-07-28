75a0e1c8d6decac8ce8e12c8d914e442dc0cebec
Jason Gong <jasongong83@gmail.com>
2021-07-28 18:01:46 -0400

    create new file for link to my-assets page. add imports and set initial state

---
+++ pages/my-assets.js
@@ -0,0 +1,17 @@
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Web3Modal from "web3modal";
+
+import { nftmarketaddress, nftaddress } from "../config";
+
+import Market from "../artifacts/contracts/Market.sol/NFTMarket.json";
+import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
+
+export default function MyAssets() {
+  const [nfts, setNfts] = useState([]);
+  const [loadingState, setLoadingState] = useState("not-loaded");
+  useEffect(() => {
+    loadNFTs();
+  }, []);
+}

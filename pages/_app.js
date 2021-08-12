55e360356d6c886d5d6f89d1b324608d36d569ba
Jason Gong <jasongong83@gmail.com>
2021-08-11 22:33:20 -0400

    add readProfile function that accesses idx to return user profile data

--- pages/_app.js
+++ pages/_app.js
@@ -23,23 +23,45 @@ function MyApp({ Component, pageProps }) {
     return addresses;
   }
 
+  async function readProfile() {
+    const [address] = await connect();
+    const ceramic = new CeramicClient(endpoint);
+    const idx = new IDX({ ceramic });
+
+    try {
+      const data = await idx.get("basicProfile", `${address}@eip155:1`);
+
+      console.log("data: ", data);
+      if (data.name) setName(data.name);
+      if (data.avatar) setImage(data.avatar);
+    } catch (error) {
+      console.log("error: ", error);
+      setLoaded(true);
+    }
+  }
+
   return (
     <div>
       <nav className="border-b p-6">
-        <p className="text-4xl font-bold">Metaroad Marketplace</p>
-        <div className="flex mt-8">
-          <Link href="/">
-            <a className="mr-8 text-purple-500">Home</a>
-          </Link>
-          <Link href="/create-item">
-            <a className="mr-8 text-purple-500">Sell NFT</a>
-          </Link>
-          <Link href="/my-assets">
-            <a className="mr-8 text-purple-500">My NFTs</a>
-          </Link>
-          <Link href="/creator-dashboard">
-            <a className="mr-8 text-purple-500">Creator Dashboard</a>
-          </Link>
+        <div>
+          <p className="text-4xl font-bold">Metaroad Marketplace</p>
+          <div className="flex mt-8">
+            <Link href="/">
+              <a className="mr-8 text-purple-500">Home</a>
+            </Link>
+            <Link href="/create-item">
+              <a className="mr-8 text-purple-500">Sell NFT</a>
+            </Link>
+            <Link href="/my-assets">
+              <a className="mr-8 text-purple-500">My NFTs</a>
+            </Link>
+            <Link href="/creator-dashboard">
+              <a className="mr-8 text-purple-500">Creator Dashboard</a>
+            </Link>
+          </div>
+        </div>
+        <div>
+          <button onClick={readProfile}>Read Profile</button>
         </div>
       </nav>
       <Component {...pageProps} />

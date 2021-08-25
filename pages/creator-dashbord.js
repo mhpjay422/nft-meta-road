3d53948d72d2c111a4945e951f4badc9f8c2095f
Jason Gong <jasongong83@gmail.com>
2021-08-24 22:33:07 -0400

    add UI for creator dashboard

--- pages/creator-dashbord.js
+++ pages/creator-dashbord.js
@@ -55,4 +55,46 @@ export default function CreatorDashboard() {
   }
   if (loadingState === "loaded" && !nfts.length)
     return <h1 className="py-10 px-20 text-3xl">No assets created</h1>;
+
+  return (
+    <div>
+      <div className="p-4">
+        <h2 className="text-2xl py-2">Items Created</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+          {nfts.map((nft, i) => (
+            <div key={i} className="border shadow rounded-xl overflow-hidden">
+              <img src={nft.image} className="rounded" />
+              <div className="p-4 bg-black">
+                <p className="text-2xl font-bold text-white">
+                  Price - {nft.price} Eth
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="px-4">
+        {Boolean(sold.length) && (
+          <div>
+            <h2 className="text-2xl py-2">Items sold</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+              {sold.map((nft, i) => (
+                <div
+                  key={i}
+                  className="border shadow rounded-xl overflow-hidden"
+                >
+                  <img src={nft.image} className="rounded" />
+                  <div className="p-4 bg-black">
+                    <p className="text-2xl font-bold text-white">
+                      Price - {nft.price} Eth
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }

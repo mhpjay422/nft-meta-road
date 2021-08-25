ae05c436168924425f64bee31127d48a2848d0a2
Jason Gong <jasongong83@gmail.com>
2021-08-25 11:20:39 -0400

    remove imports in indexjs to test in deployment

--- pages/create-item.js
+++ pages/create-item.js
@@ -9,8 +9,8 @@ const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
 
 import { nftaddress, nftmarketaddress } from "../config.js";
 
-import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
-import Market from "../artifacts/contracts/NFTMarket.sol/NFTMarket.json";
+import NFT from "../artifacts/contracts/nft.sol/nft.json";
+import Market from "../artifacts/contracts/nftmarket.sol/nftmarket.json";
 
 export default function CreateItem() {
   const [fileUrl, setFileUrl] = useState(null);
--- pages/my-assets.js
+++ pages/my-assets.js
@@ -6,8 +6,8 @@ import Web3Modal from "web3modal";
 
 import { nftmarketaddress, nftaddress } from "../config";
 
-import Market from "../artifacts/contracts/NFTMarket.sol/NFTMarket.json";
-import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
+import NFT from "../artifacts/contracts/nft.sol/nft.json";
+import Market from "../artifacts/contracts/nftmarket.sol/nftmarket.json";
 
 export default function MyAssets() {
   const [nfts, setNfts] = useState([]);

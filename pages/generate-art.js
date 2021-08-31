db90776cd628e49cda2873f1676c6628e85ef588
Jason Gong <jasongong83@gmail.com>
2021-08-31 18:57:41 -0400

    install and add p5 drawing functionality to Generate Art

--- pages/generate-art.js
+++ pages/generate-art.js
@@ -1,3 +1,12 @@
-export default function GenerateArt() {
-  return <div>hello</div>;
+import P5Wrapper from "react-p5-wrapper";
+import sketch from "../sketch";
+
+function GenerateArt(p) {
+  return (
+    <div>
+      <P5Wrapper sketch={sketch} />
+    </div>
+  );
 }
+
+export default GenerateArt;
---
+++ sketch.js
@@ -0,0 +1,18 @@
+function sketch(p) {
+  let rotation = 0;
+
+  p.setup = function () {
+    p.createCanvas(1500, 400);
+  };
+
+  p.draw = function () {
+    if (p.mouseIsPressed) {
+      p.fill(0);
+    } else {
+      p.fill(255);
+    }
+    p.ellipse(p.mouseX, p.mouseY, 100, 100);
+  };
+}
+
+export default sketch;

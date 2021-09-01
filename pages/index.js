4a82a4fd23934ee953152d1900f34b8267900718
Jason Gong <jasongong83@gmail.com>
2021-09-01 16:38:25 -0400

    install domino to create a window in production

--- pages/index.js
+++ pages/index.js
@@ -73,22 +73,16 @@ export default function Home() {
     loadNFTs();
   }
 
-  const runScript = () => {
-    if (window) {
-      return (
-        <Script
-          src="https://www.unpkg.com/p5@1.3.1/lib/p5.min.js"
-          rel="import"
-        ></Script>
-      );
-    }
-  };
-
   if (loadingState === "loaded" && !nfts.length)
     return <h1 className="px-20 py-10 text-3xl">No items in marketplace</h1>;
   return (
     <div>
-      <head>{runScript}</head>
+      <head>
+        <Script
+          src="https://www.unpkg.com/p5@1.3.1/lib/p5.min.js"
+          rel="import"
+        ></Script>
+      </head>
       <div className="flex justify-center">
         <div className="px-4" style={{ maxWidth: "1600px" }}>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">

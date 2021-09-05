545a1460828944872b3f73b6ac967e13183f46b8
Jason Gong <jasongong83@gmail.com>
2021-09-05 14:26:05 -0400

    make footer sticky

--- pages/_app.js
+++ pages/_app.js
@@ -72,64 +72,66 @@ function MyApp({ Component, pageProps }) {
   }
 
   return (
-    <div className="min-h-full grid">
-      <nav className="flex flex-row border-b p-6 justify-between">
-        <div>
-          <p className="text-4xl font-bold">Metaroad Marketplace</p>
-          <div className="flex mt-8">
-            <Link href="/">
-              <a className="mr-8 text-purple-500">Home</a>
-            </Link>
-            <Link href="/create-item">
-              <a className="mr-8 text-purple-500">Sell NFT</a>
-            </Link>
-            <Link href="/my-assets">
-              <a className="mr-8 text-purple-500">My NFTs</a>
-            </Link>
-            <Link href="/creator-dashboard">
-              <a className="mr-8 text-purple-500">Creator Dashboard</a>
-            </Link>
-            <Link href="/generate-art">
-              <a className="mr-8 text-purple-500">Generate Art</a>
-            </Link>
-          </div>
-        </div>
-        <div className="flex flex-row mt-6">
-          {name && <h3 className="mr-4 text-4xl">Hello, {name}</h3>}
-          {!image && !name && loaded && (
-            <h4>No profile, please create one...</h4>
-          )}
-          {image && <img className="w-20 h-14" src={image} />}
-        </div>
-        <div>
+    <div>
+      <div className="min-height-footer">
+        <nav className="flex flex-row border-b p-6 justify-between">
           <div>
-            <input
-              className="border-2 border-gray-400 rounded-sm"
-              placeholder="Name"
-              onChange={(e) => setName(e.target.value)}
-            />
-            <input
-              className="border-2 border-gray-400 rounded-sm ml-1 mr-1"
-              placeholder="Profile Image Url"
-              onChange={(e) => setImage(e.target.value)}
-            />
-            <button
-              onClick={readProfile}
-              className="bg-blue-400 text-white border-2 border-blue-400 ml-1 mr-1 shadow-xl rounded-lg"
-            >
-              <p className="m-1 ml-2 mr-2">Read Profile</p>
-            </button>
-            <button
-              onClick={updateProfile}
-              className="bg-blue-400 text-white border-2 border-blue-400 ml-1 mr-1 shadow-xl rounded-lg"
-            >
-              <p className="m-1 ml-2 mr-2">Set Profile</p>
-            </button>
+            <p className="text-4xl font-bold">Metaroad Marketplace</p>
+            <div className="flex mt-8">
+              <Link href="/">
+                <a className="mr-8 text-purple-500">Home</a>
+              </Link>
+              <Link href="/create-item">
+                <a className="mr-8 text-purple-500">Sell NFT</a>
+              </Link>
+              <Link href="/my-assets">
+                <a className="mr-8 text-purple-500">My NFTs</a>
+              </Link>
+              <Link href="/creator-dashboard">
+                <a className="mr-8 text-purple-500">Creator Dashboard</a>
+              </Link>
+              <Link href="/generate-art">
+                <a className="mr-8 text-purple-500">Generate Art</a>
+              </Link>
+            </div>
           </div>
-        </div>
-      </nav>
-      <Component {...pageProps} />
-      <footer className="h-28 min-h-full border-2 solid mt-10 flex flex-row ml-40 mr-40 justify-between">
+          <div className="flex flex-row mt-6">
+            {name && <h3 className="mr-4 text-4xl">Hello, {name}</h3>}
+            {!image && !name && loaded && (
+              <h4>No profile, please create one...</h4>
+            )}
+            {image && <img className="w-20 h-14" src={image} />}
+          </div>
+          <div>
+            <div>
+              <input
+                className="border-2 border-gray-400 rounded-sm"
+                placeholder="Name"
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input
+                className="border-2 border-gray-400 rounded-sm ml-1 mr-1"
+                placeholder="Profile Image Url"
+                onChange={(e) => setImage(e.target.value)}
+              />
+              <button
+                onClick={readProfile}
+                className="bg-blue-400 text-white border-2 border-blue-400 ml-1 mr-1 shadow-xl rounded-lg"
+              >
+                <p className="m-1 ml-2 mr-2">Read Profile</p>
+              </button>
+              <button
+                onClick={updateProfile}
+                className="bg-blue-400 text-white border-2 border-blue-400 ml-1 mr-1 shadow-xl rounded-lg"
+              >
+                <p className="m-1 ml-2 mr-2">Set Profile</p>
+              </button>
+            </div>
+          </div>
+        </nav>
+        <Component {...pageProps} />
+      </div>
+      <footer className="h-28 border-2 solid mt-10 flex flex-row ml-40 mr-40 justify-between">
         <div className="font-sans text-gray-800 align-middle flex self-center">
           © 2021 Metaroad Marketplace, Inc. All rights reserved
         </div>

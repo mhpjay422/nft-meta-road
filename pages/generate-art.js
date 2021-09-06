b27f0e86eb7ed2c22c0940c7185dd5c50a6f628f
Jason Gong <jasongong83@gmail.com>
2021-09-06 19:00:28 -0400

    move components to p5component

--- pages/generate-art.js
+++ pages/generate-art.js
@@ -5,16 +5,7 @@ const DynamicComponentWithNoSSR = dynamic(() => import("../p5component.js"), {
 });
 
 function GenerateArt(p) {
-  return (
-    <div>
-      <div className="h-20 bg-gray-50"></div>
-      <div className="flex align-middle justify-around border-2 solid bg-black">
-        <div className="bg-white">
-          <DynamicComponentWithNoSSR className="bg-yellow-200" />
-        </div>
-      </div>
-    </div>
-  );
+  return <DynamicComponentWithNoSSR />;
 }
 
 export default GenerateArt;

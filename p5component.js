391559a128067f22b35d97adc609f40e375cd47d
Jason Gong <jasongong83@gmail.com>
2021-09-09 23:01:05 -0400

    reformat createSale

--- p5component.js
+++ p5component.js
@@ -43,13 +43,13 @@ export default function P5component() {
     }
   }
 
-  async function createMarket(url) {
+  async function createMarket(fileUrl) {
     const { name, description, price } = formInput;
     if (!name || !description || !price) return;
     const data = JSON.stringify({
       name,
       description,
-      image: url,
+      image: fileUrl,
     });
     try {
       const added = await client.add(data);
@@ -59,6 +59,31 @@ export default function P5component() {
     } catch (error) {
       console.log("Error uploading file: ", error);
     }
+  }
+
+  async function createSale(url) {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+
+    let contract = new ethers.Contract(nftaddress, abi, signer);
+    let transaction = await contract.createToken(url);
+    let tx = await transaction.wait();
+    let event = tx.events[0];
+    let value = event.args[2];
+    let tokenId = value.toNumber();
+    const price = ethers.utils.parseUnits(formInput.price, "ether");
+
+    contract = new ethers.Contract(nftmarketaddress, marketAbi, signer);
+    let listingPrice = await contract.getListingPrice();
+    listingPrice = listingPrice.toString();
+
+    transaction = await contract.createMarketItem(nftaddress, tokenId, price, {
+      value: listingPrice,
+    });
+    await transaction.wait();
+    router.push("/");
   }
 
   return (
